e2ebf29ce218e6151b0c53cc48eb35d0126bbb31
Gary Lee <leechisan2007@hotmail.com>
2022-06-06 21:23:07 +0800

    Implement basic create and join channel

--- backend/AwsServerlessChatroom/Function.cs
+++ backend/AwsServerlessChatroom/Function.cs
@@ -5,6 +5,7 @@ using Amazon.DynamoDBv2.Model;
 using Amazon.Lambda.APIGatewayEvents;
 using Amazon.Lambda.Core;
 using System.Text;
+using System.Text.Json;
 
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
 
@@ -12,6 +13,102 @@ namespace AwsServerlessChatroom;
 
 public class Function : IDisposable
 {
+    private static async Task PushDataToClient(APIGatewayProxyRequest request, object data)
+    {
+        using var apiClient = new AmazonApiGatewayManagementApiClient(new AmazonApiGatewayManagementApiConfig
+        {
+            ServiceURL = request.GetServiceUrl(),
+        });
+
+        using var ms = new MemoryStream();
+        JsonSerializer.Serialize(ms, data);
+        ms.Position = 0;
+
+        try
+        {
+            var response = await apiClient.PostToConnectionAsync(new PostToConnectionRequest
+            {
+                ConnectionId = request.RequestContext.ConnectionId,
+                Data = ms,
+            });
+
+            if (!response.IsSuccess())
+            {
+                throw new AwsServiceException(response, $"Failed to send data to connection '{request.RequestContext.ConnectionId}'");
+            }
+        }
+        catch (GoneException)
+        {
+            // Connection no longer exists, nothing we can do
+        }
+    }
+
+    private async Task JoinChannel(APIGatewayProxyRequest request, string channelId)
+    {
+        var queryResponse = await _dynamoDbClient.QueryAsync(new QueryRequest
+        {
+            TableName = "ServerlessChatroomApi-Channels",
+            KeyConditionExpression = "ChannelId = :channelId",
+            ExpressionAttributeValues = new Dictionary<string, AttributeValue>
+            {
+                [":channelId"] = new AttributeValue { S = channelId },
+            },
+            Select = Select.COUNT,
+        });
+
+        if (!queryResponse.IsSuccess())
+        {
+            throw new AwsServiceException(queryResponse, "Cannot query from dynamodb");
+        }
+
+        var channelExists = queryResponse.Count > 0;
+        if (!channelExists)
+        {
+            await PushDataToClient(request, new { result = "Channel does not exist" });
+            return;
+        }
+
+        var putItemRequest = new PutItemRequest
+        {
+            TableName = "ServerlessChatroomApi-ChannelSubscriptions",
+            Item = new Dictionary<string, AttributeValue>
+            {
+                ["ChannelId"] = new AttributeValue { S = channelId },
+                ["ConnectionId"] = new AttributeValue { S = request.RequestContext.ConnectionId },
+            },
+        };
+
+        var response = await _dynamoDbClient.PutItemAsync(putItemRequest);
+        if (!response.IsSuccess())
+        {
+            throw new AwsServiceException(response, "Cannot put item to dynamodb");
+        }
+
+        await PushDataToClient(request, new { result = "Joining channel success" });
+    }
+
+    private async Task CreateChannel(APIGatewayProxyRequest request, string channelName)
+    {
+        var channelId = Guid.NewGuid().ToString();
+        var putItemRequest = new PutItemRequest
+        {
+            TableName = "ServerlessChatroomApi-Channels",
+            Item = new Dictionary<string, AttributeValue>
+            {
+                ["ChannelId"] = new AttributeValue { S = channelId },
+                ["Name"] = new AttributeValue { S = channelName },
+            },
+        };
+
+        var response = await _dynamoDbClient.PutItemAsync(putItemRequest);
+        if (!response.IsSuccess())
+        {
+            throw new AwsServiceException(response, "Cannot put item to dynamodb");
+        }
+
+        await PushDataToClient(request, new { result = new { channelId } });
+    }
+
     private readonly AmazonDynamoDBClient _dynamoDbClient = new();
 
     public async Task<APIGatewayProxyResponse> OnConnect(APIGatewayProxyRequest request)
@@ -64,45 +161,25 @@ public class Function : IDisposable
         };
     }
 
-    public async Task<APIGatewayProxyResponse> Default(APIGatewayProxyRequest request, ILambdaContext lambdaContext)
+    public async Task<APIGatewayProxyResponse> Default(APIGatewayProxyRequest request)
     {
-        using var apiClient = new AmazonApiGatewayManagementApiClient(new AmazonApiGatewayManagementApiConfig
+        var message = JsonDocument.Parse(request.Body);
+        switch (message.RootElement.GetProperty("method").GetString())
         {
-            ServiceURL = request.GetServiceUrl(),
-        });
-
-        using var ms = new MemoryStream(Encoding.UTF8.GetBytes("Hello"))
-        {
-            Position = 0
-        };
-        var postRequest = new PostToConnectionRequest
-        {
-            ConnectionId = request.RequestContext.ConnectionId,
-            Data = ms,
-        };
-
-        try
-        {
-            var response = await apiClient.PostToConnectionAsync(postRequest);
-            return (int)response.HttpStatusCode switch
-            {
-                >= 200 and < 300 => new APIGatewayProxyResponse
-                {
-                    StatusCode = 200,
-                    Body = $"Data sent to {request.RequestContext.ConnectionId}",
-                },
-                _ => throw new Exception("Cannot post"),
-            };
-        }
-        catch (Exception e)
-        {
-            lambdaContext.Logger.LogError(e.ToString());
+            case "createChannel":
+                await CreateChannel(request, message.RootElement.GetProperty("channelName").GetString()!);
+                break;
+            case "joinChannel":
+                await JoinChannel(request, message.RootElement.GetProperty("channelId").GetString()!);
+                break;
+            default:
+                await PushDataToClient(request, new { error = "Unsupported method" });
+                break;
         }
 
         return new APIGatewayProxyResponse
         {
-            StatusCode = 500,
-            Body = $"OK",
+            StatusCode = 200
         };
     }
 
